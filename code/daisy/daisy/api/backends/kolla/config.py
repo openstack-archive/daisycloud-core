8286d8b2f21b4f1c7387b4321ce93e81dabe941a
Zhou Ya <zhou.ya@zte.com.cn>
2016-07-11 16:03:00 +0800

    add config.py file for daisy to generate kolla config files treat all-in-one as a special case of multinode use regular expression to clean the inventory file
    Change-Id: I3d04a70726f47d00beef7fa2f307733ad300a5d4
    Signed-off-by: Zhou Ya <zhou.ya@zte.com.cn>

---
+++ code/daisy/daisy/api/backends/kolla/config.py
@@ -0,0 +1,158 @@
+# Copyright 2013 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import yaml
+import random
+import string
+import uuid
+from Crypto.PublicKey import RSA
+
+
+# generate kolla's ansible inventory multinode file
+def clean_inventory_file(filename):
+    fp = open('%s' % filename)
+    txt = fp.read()
+    fp.close()
+    node_names = ['control', 'network', 'compute',
+                  'storage', 'kibana:children']
+    for section_name in node_names[0:4]:
+        next_name_index = node_names.index('%s' % section_name)
+        match = re.search(r"\[%s\](.*)\[%s\]" % (
+            section_name,
+            node_names[next_name_index+1]),
+            txt, re.S)
+        txt = txt.replace(match.group(1), '\n\n')
+    fp = file('%s' % filename, 'w')
+    fp.write(txt)
+    fp.close()
+
+
+def update_inventory_file(filename, node_name, host_name,
+                          num_of_host, connection_type):
+    fp = file('%s' % filename)
+    lines = []
+    for line in fp:
+        lines.append(line)
+    fp.close()
+    index_of_label = lines.index('[%s]\n' % node_name)
+    lines.insert(index_of_label + num_of_host,
+                 '%s       ansible_connection=%s\n' %
+                 (host_name, connection_type))
+    s = ''.join(lines)
+    fp = file('%s' % filename, 'w')
+    fp.write(s)
+    fp.close()
+
+
+def add_role_to_inventory(config_data):
+    host_sequence = 1
+    for control_ip in config_data['control_lb_ip']:
+        update_inventory_file('multinode', 'control', control_ip, host_sequence, 'ssh')
+        host_sequence = host_sequence + 1
+
+    host_sequence = 1
+    for network_ip in config_data['network_ip']:
+        update_inventory_file('multinode', 'network', network_ip, host_sequence, 'ssh')
+        host_sequence = host_sequence + 1
+
+    host_sequence = 1
+    for compute_ip in config_data['compute_ip']:
+        update_inventory_file('multinode', 'compute', compute_ip, host_sequence, 'ssh')
+        host_sequence = host_sequence + 1
+
+    host_sequence = 1
+    for storage_ip in config_data['storage_ip']:
+        update_inventory_file('multinode', 'storage', storage_ip, host_sequence, 'ssh')
+        host_sequence = host_sequence + 1
+
+
+# generate kolla's globals.yml file
+def update_globals_yml(config_data):
+    VIP = config_data['VIP']
+    IntIfMac = config_data['IntIfMac']
+    ExtIfMac = config_data['ExtIfMac']
+    local_ip = config_data['LocalIP']
+    # kolla_yml = yaml.load(file('/etc/kolla/globals.yml'))
+    kolla_yml = {'openstack_release': '2.0.1',
+                 'docker_registry': '127.0.0.1:4000',
+                 'docker_namespace': 'kollaglue',
+                 'kolla_internal_vip_address': '10.10.10.254',
+                 'network_interface': 'eth0',
+                 'neutron_external_interface': 'eth1'
+                 }
+    kolla_yml['kolla_internal_vip_address'] = VIP.encode()
+    kolla_yml['network_interface'] = IntIfMac.encode()
+    kolla_yml['neutron_external_interface'] = ExtIfMac.encode()
+    kolla_yml['docker_registry'] = local_ip.encode()
+    yaml.dump(kolla_yml, file('/etc/kolla/globals.yml', 'w'),
+              default_flow_style=False)
+
+
+# generate kolla's password.yml file
+def generate_RSA(bits=2048):
+    new_key = RSA.generate(bits, os.urandom)
+    private_key = new_key.exportKey("PEM")
+    public_key = new_key.publickey().exportKey("OpenSSH")
+    return private_key, public_key
+
+
+def update_password_yml():
+    # These keys should be random uuids
+    uuid_keys = ['ceph_cluster_fsid', 'rbd_secret_uuid']
+
+    # SSH key pair
+    ssh_keys = ['nova_ssh_key']
+
+    # If these keys are None, leave them as None
+    blank_keys = ['docker_registry_password']
+
+    # generate the password of horizon
+    keystone_admin_password = ['keystone_admin_password']
+
+    # length of password
+    length = 40
+
+    with open('/etc/kolla/passwords.yml', 'r') as f:
+        passwords = yaml.load(f.read())
+
+    for k, v in passwords.items():
+        if (k in ssh_keys and
+                (v is None or
+                 v.get('public_key') is None and
+                 v.get('private_key') is None)):
+            private_key, public_key = generate_RSA()
+            passwords[k] = {
+                'private_key': private_key,
+                'public_key': public_key
+            }
+            continue
+        if v is None:
+            if k in blank_keys:
+                continue
+            if k in uuid_keys:
+                passwords[k] = str(uuid.uuid4())
+            if k in keystone_admin_password:
+                passwords[k] = "keystone"
+            else:
+                passwords[k] = ''.join([
+                    random.SystemRandom().choice(
+                        string.ascii_letters + string.digits)
+                    for n in range(length)
+                ])
+    f.close()
+    with open('/etc/kolla/passwords.yml', 'w') as f:
+        f.write(yaml.dump(passwords, default_flow_style=False))
+        f.close()
