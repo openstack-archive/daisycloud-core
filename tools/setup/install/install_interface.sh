f91cbfdba50011a3ce0463c24409fe1ff8bb63e8
luyao <lu.yao135@zte.com.cn>
2016-08-16 16:37:38 +0800

    when we use simplier centos, may lose these rpms which are necessary
    Change-Id: Ia54f808c41bd4377f0e7fab2bbab5481db173443
    Signed-off-by: luyao <lu.yao135@zte.com.cn>

--- tools/setup/install/install_interface.sh
+++ tools/setup/install/install_interface.sh
@@ -286,7 +286,7 @@ function all_install
     config_get_node_info
 
     write_install_log "Daisy Install Successfull..."
-    
+
     config_file="/home/daisy_install/daisy.conf"
     [ ! -e $config_file ] && return
     get_config "$config_file" default_backend_types
@@ -294,7 +294,7 @@ function all_install
     kolla=`echo $default_backend_types_params|grep 'kolla'|wc -l`
     if [ $kolla -ne 0 ];then
         write_install_log "Begin install kolla and depends..."
-        kolla_install  
+        kolla_install
     fi
 }
 _INSTALL_INTERFACE_FILE="install_interface.sh"
