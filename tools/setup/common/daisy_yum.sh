5c50b222968b36b7c56b59e72b65d6df3e8a3fc6
YaoLu <lu.yao135@zte.com.cn>
2016-09-22 20:30:08 +0800

    delete unused code about creat yum
    Change-Id: I1451ab8afd21aefe9181a508ae8a91a311b1d133
    Signed-off-by: YaoLu <lu.yao135@zte.com.cn>

--- tools/setup/common/daisy_yum.sh
+++ tools/setup/common/daisy_yum.sh
@@ -2,18 +2,6 @@
 
 if [ ! "$_DAISY_YUM_FILE" ];then
 
-function make_yum_server
-{
-    path=`pwd`
-    createrepo --update $path &>/dev/null
-    [ "$?" -ne 0 ] && { echo "createrepo in $path failed"; exit 1; }
-    patch_path="/home/daisy_patch/"
-    if [ -d $patch_path ];then
-        [ -d $patch_path/repodata ] && rm -rf $patch_path/repodata
-        createrepo --update $patch_path &>/dev/null
-        [ "$?" -ne 0 ] && { echo "createrepo in $patch_path failed"; exit 1; }
-    fi
-}
 
 function make_yum_client
 {
@@ -25,12 +13,7 @@ function make_yum_client
     os_iso_repofile=/etc/yum.repos.d/OS_ISO.repo
     tfg_depend_repofile=/etc/yum.repos.d/TFG_DEPEND_PACKAGE.repo
     echo -e "[$repo_name]\nname=$repo_name\nbaseurl=file:$path\nenabled=1\ngpgcheck=0" > $daisy_repofile
-    if [ -d $patch_path ];then
-    echo -e "[$patch_repo_name]\nname=$patch_repo_name\nbaseurl=file:$patch_path\nenabled=1\ngpgcheck=0" > $daisy_depend_repofile
-        daisy_yum="yum -y --disablerepo=* --enablerepo=$repo_name --enablerepo=$patch_repo_name"
-    else
-        daisy_yum="yum -y --disablerepo=* --enablerepo=$repo_name"
-    fi
+    daisy_yum="yum -y --disablerepo=* --enablerepo=$repo_name"
     if [ -f "$os_iso_repofile" ];then
         daisy_yum="${daisy_yum}"" --enablerepo=OS_ISO"
     fi
@@ -38,13 +21,6 @@ function make_yum_client
         daisy_yum="${daisy_yum}"" --enablerepo=TFG_DEPEND_PACKAGE"
     fi
     yum clean all &>/dev/null
-    yum install -y httpd
-    systemctl enable httpd
-    systemctl start httpd || { echo "httpd start failed.";  exit 1; }
-    httpd_path="/var/www/html/daisy_install"
-    chmod 777 $path
-    rm -f $httpd_path
-    ln -s $path $httpd_path
 }
 function yum_set
 {
@@ -52,7 +28,6 @@ function yum_set
     rm -rf /etc/yum.repos.d/daisy*
     echo "creating yum repo, please wait for several seconds..."
     yum install -y createrepo
-    make_yum_server
     make_yum_client
     echo "creating epel yum repo, please wait for several seconds..."
     yum install -y epel-release
