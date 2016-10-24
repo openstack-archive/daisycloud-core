67ef05878a0f84608287f9078e28930229e29eaa
Zhou Ya <zhou.ya@zte.com.cn>
2016-10-24 16:00:08 +0800

    fix template description err
    It raises an error when description set to multiple lines of
    generating cluster template.
    
    TrivialFix
    
    Change-Id: I3044f5a717a6c080029900ae6872c96380621e3c

--- code/daisyclient/daisyclient/v1/components.py
+++ code/daisyclient/daisyclient/v1/components.py
@@ -275,7 +275,7 @@ class ComponentManager(base.ManagerWithFind):
 
         hdrs = self._component_meta_to_headers(fields)
         resp, body = self.client.post('/v1/components',
-                                      headers=hdrs,
+                                      headers=None,
                                       data=hdrs)
         return_request_id = kwargs.get('return_req_id', None)
         if return_request_id is not None:
