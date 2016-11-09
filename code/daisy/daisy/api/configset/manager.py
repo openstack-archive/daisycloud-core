16d6e714f1969e08b12d7850d8e3b46738c23d28
Zhou Ya <zhou.ya@zte.com.cn>
2016-11-09 17:14:42 +0800

    add push_origin_config_to_host function
    Change-Id: I20c99e6f41de9f3bd85109cc4c5d1e4de6b39398

--- code/daisy/daisy/api/configset/manager.py
+++ code/daisy/daisy/api/configset/manager.py
@@ -22,3 +22,49 @@ class configBackend():
         for host_id in host_ids:
             self._instance.push_host_configs(host_id,
                                              component_names)
+
+    def push_origin_config_to_host(self, host_config):
+        """
+        push original config to remote host.
+        :param host_config: The config of host
+            {
+                "ip":"the ip address of remote host",
+                "config_set": [{
+                    "section": "the section name, only if the file format is
+                                SKV, the value is required.",
+                    "key": "the key of config item, the value is required.",
+                    "value": "the value of config item, if delete only key of
+                              config item, the value don't fill in,
+                              otherwise the value is required.",
+                    "old_value": "the old value of config item,
+                                  only if the action is set and duplicate key,
+                                  the value is required."
+                    "config_file": "the config file, the value is required.",
+                    "action": "the action of config item,
+                               format is add/delete/set, the value
+                               is required.",
+                    "services": "the service name list of config item,
+                                 only force type is service, the value
+                                 is required",
+                    "force_type": "the force type of config item,
+                                  if no force service/node,
+                                  the value don't fill in,
+                                  otherwise  the value is service/node",
+                    "file_format": "the file format of config item,
+                                    SKV/others,the value is required,
+                                    SKV is section/key/value.",
+                    "separator": "the separator of config item,
+                                  only if the file format is others,
+                                  the value is required"
+                },...]
+            }
+        :return: Raise Invalid exception if host config is invalid
+                 Raise webob.exc.HTTPServerError if call process error
+        """
+        self._instance.push_origin_config_to_host(host_config)
+
+    def get_host_config_progress(self, host_config):
+        return 100
+
+    def get_config_by_host(self, host_id, template_configs):
+        return self._instance.get_host_configs(host_id, template_configs)
