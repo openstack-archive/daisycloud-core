bd6ae775f2d3e6564726e843a14abdffe2ea1172
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2016-11-26 04:18:44 -0500

    Use tox to run unit test
    Prev we use:
    source /path/to/somewhere/.venv/bin/activate
    cd $WORKSPACE/code/daisy/daisy/tests
    python -m unittest discover
    
    With this patch, we can now use tox -epy27 to run our unit test.
    
    Change-Id: I71c30cf319ad15d778165bd6f787a1d48df838ac
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- code/daisy/daisy/cmd/api.py
+++ code/daisy/daisy/cmd/api.py
@@ -27,14 +27,12 @@ import sys
 import eventlet
 
 from daisy.common import utils
-import glance_store
 from oslo_config import cfg
 from oslo_log import log as logging
 import osprofiler.notifier
 import osprofiler.web
 
 from daisy.common import config
-from daisy.common import exception
 from daisy.common import wsgi
 from daisy import notifier
 from oslo_service import systemd
@@ -56,14 +54,9 @@ CONF = cfg.CONF
 CONF.import_group("profiler", "daisy.common.wsgi")
 logging.register_options(CONF)
 
-KNOWN_EXCEPTIONS = (RuntimeError,
-                    exception.WorkerCreationFailure,
-                    glance_store.exceptions.BadStoreConfiguration)
-
 
 def fail(e):
-    global KNOWN_EXCEPTIONS
-    return_code = KNOWN_EXCEPTIONS.index(type(e)) + 1
+    return_code = 100  # TODO: (huzhj) make this a meaningful value
     sys.stderr.write("ERROR: %s\n" % utils.exception_to_str(e))
     sys.exit(return_code)
 
@@ -84,13 +77,12 @@ def main():
         else:
             osprofiler.web.disable()
 
-        server = wsgi.Server(initialize_glance_store=True)
+        server = wsgi.Server()
         server.start(config.load_paste_app('daisy-api'), default_port=9292)
         systemd.notify_once()
         server.wait()
-    except KNOWN_EXCEPTIONS as e:
+    except Exception as e:
         fail(e)
-
 
 if __name__ == '__main__':
     main()
