cd888eaa25465e8e7d1bc2f42d2ed2ab4549c9db
10190269 <wu.hanlin@zte.com.cn>
2016-12-11 21:50:50 +0800

    if have no file named know_hosts,needn't delete management ip
    Change-Id: I31e9d42d9c3bd0ed0c013df6308d739e41103084

--- code/daisy/daisy/api/backends/common.py
+++ code/daisy/daisy/api/backends/common.py
@@ -190,6 +190,22 @@ def subprocess_call(command, file=None):
               "error message: %s." % (command, e.output.strip())
         raise exception.SubprocessCmdFailed(message=msg)
 
+def check_file_whether_exist(file_name):
+    #used in os.py mainly
+    try:
+        subprocess.check_output("test -f %s" % file_name, shell=True,
+                            stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 1:
+            msg = "%s does not exist" % file_name
+            LOG.info(msg)
+            return False
+        else:
+            msg = "command execute failed"
+            LOG.error(msg)
+            exception.SubprocessCmdFailed(message=msg)
+    else:
+        return True
 
 def get_host_detail(req, host_id):
     try:
