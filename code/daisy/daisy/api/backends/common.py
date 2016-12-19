eb0bdc95aead069c261f4cdd2afc29d4be14c719
10190291 <zhang.hui1@zte.com.cn>
2016-12-19 11:48:16 +0800

    storage netplane which is hid not assigned to ssh discover host
    when storage netplane is hid in creating or modifying cluster web page,
    storage netplane should not be assigned to ssh discover host
    
    Change-Id: Iedc2d2ed15cdb59e686744e18d924e7baddc5e8e

--- code/daisy/daisy/api/backends/common.py
+++ code/daisy/daisy/api/backends/common.py
@@ -965,10 +965,13 @@ def check_vlan_nic_and_join_vlan_network(req, cluster_id,
                     # physical nic name.
                     vlan_id = nic_name_list[len(nic_name_list) - 1]
                     nic_name = interface_info['name'][: -len(vlan_id) - 1]
+                    exclude_networks = ['DATAPLANE', 'EXTERNAL']
+                    use_share_disk = if_used_shared_storage(req, cluster_id)
+                    if not use_share_disk:
+                        exclude_networks.append('STORAGE')
 
                     for network in networks:
-                        if network['network_type'] in ['DATAPLANE',
-                                                       'EXTERNAL']:
+                        if network['network_type'] in exclude_networks:
                             continue
                         network_cidr = network.get('cidr', None)
                         if network_cidr:
@@ -1047,9 +1050,13 @@ def check_bond_or_ether_nic_and_join_network(req,
                                   % host_info_ip
                             LOG.error(msg)
                             raise exception.Forbidden(msg)
+                        exclude_networks = ['DATAPLANE', 'EXTERNAL']
+                        use_share_disk = if_used_shared_storage(req,
+                                                                cluster_id)
+                        if not use_share_disk:
+                            exclude_networks.append('STORAGE')
                         for network in networks:
-                            if network['network_type'] in ['DATAPLANE',
-                                                           'EXTERNAL']:
+                            if network['network_type'] in exclude_networks:
                                 continue
                             if network.get('cidr', None):
                                 ip_in_cidr = utils.is_ip_in_cidr(
@@ -1094,3 +1101,22 @@ def check_bond_or_ether_nic_and_join_network(req,
                 LOG.info("add the host %s join the cluster %s and"
                          " assigned_network successful" %
                          (host_id, cluster_id))
+
+
+def if_used_shared_storage(req, cluster_id):
+    cluster_roles = get_cluster_roles_detail(req, cluster_id)
+    cluster_backends = set([role['deployment_backend']
+                            for role in cluster_roles])
+    for backend in cluster_backends:
+        try:
+            backend_disk = importutils.import_module(
+                'daisy.api.backends.%s.disk_array' % backend)
+        except Exception:
+            return False
+        else:
+            if hasattr(backend_disk, 'get_disk_array_info'):
+                disks_info = backend_disk.get_disk_array_info(req, cluster_id)
+                for info in disks_info:
+                    if info:
+                        return True
+    return False
