5fae88d7dd98a44e2906921df34cdc71d7f85da8
Yao Lu <lu.yao135@zte.com.cn>
2016-12-27 17:25:10 +0800

    patch of 412261,solve config net error
    Change-Id: I5e6880fe8766de4715939a70173ce9356f957761
    Signed-off-by: Yao Lu <lu.yao135@zte.com.cn>

---
+++ code/daisy/daisy/tests/api/backends/osinstall/pxe/test_install.py
@@ -0,0 +1,42 @@
+import mock
+import webob
+from daisy import test
+from daisy.context import RequestContext
+import subprocess
+from daisy.api.backends.osinstall.pxe import install
+
+
+def subprocesscall(cmd):
+    subprocess.call(cmd, shell=True,
+                    stdout=open('/dev/null', 'w'),
+                    stderr=subprocess.STDOUT)
+
+
+class OSInstall(test.TestCase):
+
+    def setUp(self):
+        super(OSInstall, self).setUp()
+        self.req = webob.Request.blank('/')
+        self.req.context = RequestContext(is_admin=True, user='fake user',
+                                          tenant='fake tenant')
+
+    @mock.patch('daisy.api.backends.common.update_db_host_status')
+    @mock.patch('subprocess.check_output')
+    @mock.patch('daisy.api.backends.common.subprocess_call')
+    def test__os_thread_bin(self, mock_subprocess_call,
+                            mock_do_check_output, mock_do_db_host_status):
+        cmd = 'mkdir -p /var/log/daisy/daisy_update/'
+        subprocesscall(cmd)
+        mock_do_check_output.return_value = 'upgrade OS successfully'
+        mock_subprocess_call.return_value = ''
+        host_ip = '127.0.0.1'
+        host_id = ''
+        update_file = ''
+        update_script = ''
+        install._os_thread_bin(self.req, host_ip, host_id, update_file,
+                               update_script)
+        log_file = '/var/log/daisy/daisy_update/127.0.0.1_update_os.log'
+        all_the_text = open('%s' % log_file).read()
+        self.assertIn('upgrade OS successfully', all_the_text)
+        cmd = 'rm -rf /var/log/daisy'
+        subprocesscall(cmd)
