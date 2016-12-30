8a85d50df60b6f24b9e38eacdf6e22f49dcef0c3
Zhou Ya <zhou.ya@zte.com.cn>
2016-12-30 15:19:28 +0800

    use try except to judge whether to import provider_client
    Change-Id: I9b9aef277b3f3329dc0fe0ec9f6aae4b79acad50

--- code/daisy/daisy/tests/api/backends/osinstall/pxe/test_install.py
+++ code/daisy/daisy/tests/api/backends/osinstall/pxe/test_install.py
@@ -1,9 +1,10 @@
 import mock
+import unittest
 import webob
-from daisy import test
+import exceptions
 from daisy.context import RequestContext
 import subprocess
-from daisy.api.backends.osinstall.pxe import install
+from daisy.api.backends.osinstall.pxe import install as os
 
 
 def subprocesscall(cmd):
@@ -12,13 +13,39 @@ def subprocesscall(cmd):
                     stderr=subprocess.STDOUT)
 
 
-class OSInstall(test.TestCase):
+class MockLoggingHandler():
+    """Mock logging handler to check for expected logs.
+
+    Messages are available from an instance's ``messages`` dict, in order,
+    indexed by a lowercase log level string (e.g., 'debug', 'info', etc.).
+    """
+
+    def __init__(self):
+        self.messages = {'debug': [], 'info': [], 'warning': [], 'error': []}
+
+    def info(self, message, *args, **kwargs):
+        self.messages['info'].append(message)
+
+    def error(self, message, *args, **kwargs):
+        self.messages['error'].append(message)
+
+    def reset(self):
+        for message in self.messages:
+            del self.messages[message][:]
+
+
+class TestOs(unittest.TestCase):
+    _log_handler = MockLoggingHandler()
+    _log_messages = _log_handler.messages
 
     def setUp(self):
-        super(OSInstall, self).setUp()
+        super(TestOs, self).setUp()
         self.req = webob.Request.blank('/')
         self.req.context = RequestContext(is_admin=True, user='fake user',
                                           tenant='fake tenant')
+
+    def tearDown(self):
+        super(TestOs, self).tearDown()
 
     @mock.patch('daisy.api.backends.common.update_db_host_status')
     @mock.patch('subprocess.check_output')
@@ -33,10 +60,116 @@ class OSInstall(test.TestCase):
         host_id = ''
         update_file = ''
         update_script = ''
-        install._os_thread_bin(self.req, host_ip, host_id, update_file,
-                               update_script)
+        os._os_thread_bin(self.req, host_ip, host_id, update_file,
+                          update_script)
         log_file = '/var/log/daisy/daisy_update/127.0.0.1_update_os.log'
         all_the_text = open('%s' % log_file).read()
         self.assertIn('upgrade OS successfully', all_the_text)
         cmd = 'rm -rf /var/log/daisy'
         subprocesscall(cmd)
+
+    @mock.patch('subprocess.check_output')
+    def test_get_host_location_of_cisco(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "ipmi_addr": "127.0.0.1",
+            "id": "123456789"
+        }
+        mock_check_output.return_value = "chassis-1/blade-1"
+        location = os.get_host_location_of_cisco(host_detail)
+        self.assertRaises(mock_check_output.called)
+        self.assertEqual("1/1", location)
+
+    @mock.patch('subprocess.check_output')
+    def test_get_host_location_of_cisco_with_error_cmd(self,
+                                                       mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "ipmi_addr": "127.0.0.1",
+            "id": "123456789"
+        }
+        mock_check_output.side_effect = exceptions.Exception
+        self.assertRaises(mock_check_output.called)
+        self.assertRaises(exceptions.Exception,
+                          os.get_host_location_of_cisco,
+                          host_detail)
+
+    @mock.patch('subprocess.check_output')
+    def test_set_pxe_start_of_cisco(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "location": "1/1",
+            "id": "123456789"
+        }
+        mock_check_output.return_value = "ok"
+        os.set_pxe_start_of_cisco(host_detail)
+        self.assertRaises(mock_check_output.called)
+
+    @mock.patch('subprocess.check_output')
+    def test_set_pxe_start_of_cisco_with_error_cmd(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "location": "1/1",
+            "id": "123456789"
+        }
+        mock_check_output.side_effect = exceptions.Exception
+        self.assertRaises(mock_check_output.called)
+        self.assertRaises(exceptions.Exception,
+                          os.set_pxe_start_of_cisco,
+                          host_detail)
+
+    @mock.patch('subprocess.check_output')
+    def test_set_reboot_of_cisco(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "location": "1/1",
+            "id": "123456789"
+        }
+        mock_check_output.return_value = "ok"
+        os.set_reboot_of_cisco(host_detail)
+        self.assertRaises(mock_check_output.called)
+
+    @mock.patch('subprocess.check_output')
+    def test_set_reboot_of_cisco_with_error_cmd(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "location": "1/1",
+            "id": "123456789"
+        }
+        mock_check_output.side_effect = exceptions.Exception
+        self.assertRaises(mock_check_output.called)
+        self.assertRaises(exceptions.Exception,
+                          os.set_reboot_of_cisco,
+                          host_detail)
+
+    @mock.patch('subprocess.check_output')
+    def test_set_disk_start_of_cisco(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "location": "1/1",
+            "id": "123456789"
+        }
+        mock_check_output.return_value = "ok"
+        os.set_disk_start_of_cisco(host_detail)
+        self.assertRaises(mock_check_output.called)
+
+    @mock.patch('subprocess.check_output')
+    def test_set_disk_start_of_cisco_with_error_cmd(self, mock_check_output):
+        host_detail = {
+            "ipmi_passwd": "zteroot",
+            "ipmi_user": "superuser",
+            "location": "1/1",
+            "id": "123456789"
+        }
+        mock_check_output.side_effect = exceptions.Exception
+        self.assertRaises(mock_check_output.called)
+        self.assertRaises(exceptions.Exception,
+                          os.set_disk_start_of_cisco,
+                          host_detail)
