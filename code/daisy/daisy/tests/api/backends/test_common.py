4671ddd1a1d90204eeda86869956abc3b95ceb6c
10190291 <zhang.hui1@zte.com.cn>
2017-01-22 15:27:20 +0800

    storage netplane which is hid not assigned to ssh discover host (stable/mitaka)
    Change-Id: I24aba9df799138fb8d610f721372bc282641d316

--- code/daisy/daisy/tests/api/backends/test_common.py
+++ code/daisy/daisy/tests/api/backends/test_common.py
@@ -3,6 +3,18 @@ from daisy.api.backends import common
 from daisy import test
 
 
+class DottableDict(dict):
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
+    def allowDotting(self, state=True):
+        if state:
+            self.__dict__ = self
+        else:
+            self.__dict__ = dict()
+
+
 class MockLoggingHandler(object):
 
     """Mock logging handler to check for expected logs.
@@ -45,3 +57,34 @@ class TestCommon(test.TestCase):
         common.trust_me(ip, passwd)
         self.assertIn("Setup trust to '127.0.0.1' successfully",
                       self._log_messages['info'])
+
+
+    @mock.patch('oslo_utils.importutils.import_module')
+    @mock.patch('daisy.api.backends.common.get_cluster_roles_detail')
+    def test_if_used_shared_storage_with_share(self, mock_get_role,
+                                               mock_import_module):
+        def get_disk_array_info(*args, **kwargs):
+            return ['test'], {}, ()
+        obj = DottableDict({'get_disk_array_info': get_disk_array_info})
+        mock_get_role.return_value = [{'name': 'CONTROLLER_HA',
+                                       'deployment_backend': 'tecs',
+                                       'id': '123'}]
+        mock_import_module.return_value = obj
+        req = webob.Request.blank('/')
+        use_share_disk = common.if_used_shared_storage(req, '123')
+        self.assertEqual(use_share_disk, True)
+
+    @mock.patch('oslo_utils.importutils.import_module')
+    @mock.patch('daisy.api.backends.common.get_cluster_roles_detail')
+    def test_if_used_shared_storage_without_share(self, mock_get_role,
+                                                  mock_import_module):
+        def get_disk_array_info(*args, **kwargs):
+            return [], {}, ()
+        obj = DottableDict({'get_disk_array_info': get_disk_array_info})
+        mock_get_role.return_value = [{'name': 'CONTROLLER_HA',
+                                       'deployment_backend': 'tecs',
+                                       'id': '123'}]
+        mock_import_module.return_value = obj
+        req = webob.Request.blank('/')
+        use_share_disk = common.if_used_shared_storage(req, '123')
+        self.assertEqual(use_share_disk, False)
