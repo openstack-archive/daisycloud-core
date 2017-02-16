ffffc5747880261e1d11870b280c6115c347b187
tanlinfeng <tan.linfeng@zte.com.cn>
2017-02-16 15:29:51 +0800

    Jira: Daisy-482 optimize the question that wait a long time for os installing
    Change-Id: I3b74036ab2e7d160a5595dd797f7f3b6e7dce916

--- code/daisy/daisy/tests/api/backends/osinstall/pxe/test_install.py
+++ code/daisy/daisy/tests/api/backends/osinstall/pxe/test_install.py
@@ -4,6 +4,25 @@ from daisy import test
 from daisy.context import RequestContext
 import subprocess
 from daisy.api.backends.osinstall.pxe import install
+from daisy.db.sqlalchemy import api
+from daisy.common import exception
+
+
+def update_host_meta(req):
+    host_ref = {'id': '1',
+                'name': 'host_1',
+                'tecs_version_id': '1',
+                'os_status': 'active',
+                'os_progress': 100}
+    host_id = '1'
+    host_meta = {'os_message': 'Invalid ipmi information '
+                               'configed for host',
+                 'os_status': 'install-failed'}
+    api.host_add(req.context, host_ref)
+    host_update_info = api.host_update(req.context,
+                                       host_id,
+                                       host_meta)
+    return host_update_info
 
 
 def subprocesscall(cmd):
@@ -12,13 +31,41 @@ def subprocesscall(cmd):
                     stderr=subprocess.STDOUT)
 
 
-class OSInstall(test.TestCase):
+class MockLoggingHandler():
+
+    """Mock logging handler to check for expected logs.
+
+    Messages are available from an instance's ``messages`` dict, in order,
+    indexed by a lowercase log level string (e.g., 'debug', 'info', etc.).
+    """
+
+    def __init__(self):
+        self.messages = {'debug': [], 'info': [], 'warning': [], 'error': []}
+
+    def debug(self, message, *args, **kwargs):
+        self.messages['debug'].append(message)
+
+    def info(self, message, *args, **kwargs):
+        self.messages['info'].append(message)
+
+    def error(self, message, *args, **kwargs):
+        self.messages['error'].append(message)
+
+    def reset(self):
+        for message in self.messages:
+            del self.messages[message][:]
+
+
+class TestOsInstall(test.TestCase):
+    _log_handler = MockLoggingHandler()
+    _log_messages = _log_handler.messages
 
     def setUp(self):
-        super(OSInstall, self).setUp()
+        super(TestOsInstall, self).setUp()
         self.req = webob.Request.blank('/')
         self.req.context = RequestContext(is_admin=True, user='fake user',
                                           tenant='fake tenant')
+        self._log_handler.reset()
 
     @mock.patch('daisy.api.backends.common.update_db_host_status')
     @mock.patch('subprocess.check_output')
@@ -40,3 +87,97 @@ class OSInstall(test.TestCase):
         self.assertIn('upgrade OS successfully', all_the_text)
         cmd = 'rm -rf /var/log/daisy'
         subprocesscall(cmd)
+
+    @mock.patch("daisy.api.backends.common.set_role_status_and_progress")
+    @mock.patch("daisy.api.backends.common.update_db_host_status")
+    @mock.patch('logging.Logger')
+    def test_begin_install_os_with_error_ipmi(self,
+                                              mock_log,
+                                              mock_update_host,
+                                              mock_set_role):
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        cluster_id = '1'
+
+        def set_role(req, cluster_id, opera, status, backend_name, host_id):
+            pass
+
+        hosts_detail = [{'os_version_file': 'tecs',
+                         'memory': {'total': 102400},
+                         'disks': {'sda': {'disk': 'abc',
+                                           'removable': 'abc',
+                                           'name': 'sda',
+                                           'size': '102400'}},
+                         'system': {'manufacturer': 'abc'},
+                         'ipmi_addr': '',
+                         'ipmi_user': 'zteroot',
+                         'ipmi_passwd': 'superuser',
+                         'id': '1',
+                         'swap_lv_size': 51200}]
+        mock_log.side_effect = self._log_handler
+        mock_set_role.side_effect = set_role
+        mock_update_host.return_value = {}
+        install.OSInstall(req, cluster_id, False).\
+            _begin_install_os(hosts_detail, cluster_id)
+        self.assertTrue(mock_set_role.called)
+
+    @mock.patch("daisy.api.backends.osinstall.pxe."
+                "install.OSInstall._set_power_reset")
+    @mock.patch("daisy.api.backends.osinstall.pxe."
+                "install.OSInstall._install_os_for_baremetal")
+    @mock.patch("daisy.api.backends.osinstall.pxe."
+                "install.OSInstall._set_boot_pxe")
+    @mock.patch("daisy.api.backends.common.update_db_host_status")
+    @mock.patch('logging.Logger')
+    def test_begin_install_os_with_write_ipmi(self, mock_log,
+                                              mock_update_host,
+                                              mock_set_boot_pxe,
+                                              mock_install_os_for_baremetal,
+                                              mock_set_power_reset):
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        cluster_id = '1'
+
+        class InstallMock():
+            def __init__(self):
+                pass
+
+            def install_os_for_baremetal(self, host_detail):
+                pass
+
+            def set_power_reset(self, host_detail):
+                pass
+
+        hosts_detail = [{'id': '1'}]
+        mock_log.side_effect = self._log_handler
+        mock_update_host.return_value = {}
+        mock_set_boot_pxe.return_value = True
+        mock_install_os_for_baremetal.side_effect = \
+            InstallMock().install_os_for_baremetal
+        mock_set_power_reset.side_effect = InstallMock().set_power_reset
+        install.OSInstall(req, cluster_id, False).\
+            _begin_install_os(hosts_detail, cluster_id)
+        self.assertTrue(mock_install_os_for_baremetal.called)
+        self.assertTrue(mock_set_power_reset.called)
+
+    @mock.patch("daisy.api.backends.common.subprocess_call")
+    def test_install_os_for_baremetal_with_no_version_file(self,
+                                                           mock_subprocess):
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+
+        def subprocess_return(cmd):
+            msg = "execute cmd failed by subprocess call"
+            raise exception.SubprocessCmdFailed(message=msg)
+        cluster_id = '1'
+        host_detail = {'os_version_file': 'mimosa.iso'}
+        mock_subprocess.side_effect = subprocess_return
+        self.assertRaises(
+            exception.NotFound,
+            install.OSInstall(req,
+                              cluster_id,
+                              False)._install_os_for_baremetal,
+            host_detail)
