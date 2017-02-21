65e00ad86ec2bf316014a30a45b51aedd442ac9b
meizhifang <mei.zhifang97@zte.com.cn>
2017-02-21 13:07:08 +0800

    get numa node from dvsc cpus
    Change-Id: I9f6a66f06541e23f44b22a0af06556b38616d0d4

--- code/daisy/daisy/tests/common/test_utils.py
+++ code/daisy/daisy/tests/common/test_utils.py
@@ -52,6 +52,31 @@ class TestUtils(test.TestCase):
         self.assertRaises(exception.Invalid,
                           utils.get_numa_node_from_cpus, numa_cpus, cpus_str)
 
+    def test_get_numa_node_from_dvsc_cpus(self):
+        node0_cpus = range(0, 6) + range(12, 18)
+        node1_cpus = range(6, 12) + range(18, 24)
+        numa_cpus = {'numa_node0': node0_cpus,
+                     'numa_node1': node1_cpus, }
+        dvsc_cpus = '3,4'
+        real_numas = utils.get_numa_node_from_dvsc_cpus(numa_cpus, dvsc_cpus)
+        expect_numas = [0]
+        self.assertEqual(real_numas, expect_numas)
+
+        dvsc_cpus = '6-7'
+        real_numas = utils.get_numa_node_from_dvsc_cpus(numa_cpus, dvsc_cpus)
+        expect_numas = [1]
+        self.assertEqual(real_numas, expect_numas)
+
+    def test_get_numa_node_from_dvsc_cpus_with_exception(self):
+        dvsc_cpus = '6'
+        node0_cpus = range(0, 6) + range(12, 18)
+        node1_cpus = range(6, 12) + range(18, 24)
+        numa_cpus = {'numa_node0': node0_cpus,
+                     'numa_nod': node1_cpus, }
+        self.assertRaises(exception.Invalid,
+                          utils.get_numa_node_from_dvsc_cpus,
+                          numa_cpus, dvsc_cpus)
+
     def test_cidr_convert_to_ip_ranges(self):
         cidr = '192.168.1.25/31'
         ip_range = utils.cidr_convert_to_ip_ranges(cidr)
