57c7caf5f1b41a884f99ab15a2d27b859a19dd0d
meizhifang <mei.zhifang97@zte.com.cn>
2017-02-22 08:27:04 +0800

    When begin to install os by pxe, progress is 100%
    Change-Id: Ic8cda0161dd52c99765daa76b2a9a3320ff4aa76

--- code/daisy/daisy/tests/api/backends/test_common.py
+++ code/daisy/daisy/tests/api/backends/test_common.py
@@ -2,7 +2,7 @@ import mock
 from daisy.api.backends import common
 from daisy import test
 import webob
-
+from daisy.context import RequestContext
 
 class DottableDict(dict):
     def __init__(self, *args, **kwargs):
@@ -88,3 +88,88 @@ class TestCommon(test.TestCase):
         req = webob.Request.blank('/')
         use_share_disk = common.if_used_shared_storage(req, '123')
         self.assertEqual(use_share_disk, False)
+
+    @mock.patch('daisy.registry.client.v1.api.update_host_metadata')
+    def test_update_db_host_status(self, mock_do_update_host):
+        def mock_update_host(*args, **kwargs):
+            return host_status
+
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        host_id = '123'
+        host_status = {"os_progress": 100, "os_status": "active",
+                       "messages": "test"}
+        mock_do_update_host.side_effect = mock_update_host
+        host_info = common.update_db_host_status(req, host_id, host_status)
+        self.assertEqual(100, host_info['os_progress'])
+        self.assertEqual("active", host_info['os_status'])
+        self.assertEqual("test", host_info['messages'])
+
+    @mock.patch('daisy.registry.client.v1.api.update_host_metadata')
+    def test_update_db_host_status_with_progress_is_zero(self,
+                                                         mock_do_update_host):
+        def mock_update_host(*args, **kwargs):
+            return host_status
+
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        host_id = '123'
+        host_status = {"os_progress": 0, "os_status": "init",
+                       "messages": "test"}
+        mock_do_update_host.side_effect = mock_update_host
+        host_info = common.update_db_host_status(req, host_id, host_status)
+        self.assertEqual(0, host_info['os_progress'])
+        self.assertEqual("init", host_info['os_status'])
+        self.assertEqual("test", host_info['messages'])
+
+    @mock.patch('daisy.registry.client.v1.api.update_host_metadata')
+    def test_update_db_host_status_with_version_id(self, mock_do_update_host):
+        def mock_update_host(*args, **kwargs):
+            return host_status
+
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        host_id = '123'
+        version_id = "456"
+        host_status = {"os_progress": 100, "os_status": "active",
+                       "os_version_id": version_id}
+        mock_do_update_host.side_effect = mock_update_host
+        host_info = common.update_db_host_status(req, host_id, host_status,
+                                                 version_id)
+        self.assertEqual("456", host_info['os_version_id'])
+
+    @mock.patch('daisy.registry.client.v1.api.update_host_metadata')
+    def test_update_db_host_status_with_tecs_patch_id(self,
+                                                      mock_do_update_host):
+        def mock_update_host(*args, **kwargs):
+            return host_status
+
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        host_id = '123'
+        tecs_patch_id = "456"
+        host_status = {"os_progress": 100, "os_status": "active",
+                       "tecs_patch_id": tecs_patch_id}
+        mock_do_update_host.side_effect = mock_update_host
+        host_info = common.update_db_host_status(req, host_id, host_status)
+        self.assertEqual("456", host_info['tecs_patch_id'])
+
+    @mock.patch('daisy.registry.client.v1.api.update_host_metadata')
+    def test_update_db_host_status_with_os_patch_id(self, mock_do_update_host):
+        def mock_update_host(*args, **kwargs):
+            return host_status
+
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        host_id = '123'
+        version_patch_id = "456"
+        host_status = {"os_progress": 100, "os_status": "active",
+                       "version_patch_id": version_patch_id}
+        mock_do_update_host.side_effect = mock_update_host
+        host_info = common.update_db_host_status(req, host_id, host_status)
+        self.assertEqual("456", host_info['version_patch_id'])
