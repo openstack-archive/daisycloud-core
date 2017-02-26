11e48bffedf0f3cb29358d6703309c22ca1ab162
tanlinfeng <tan.linfeng@zte.com.cn>
2017-02-27 05:45:41 +0800

    delete tecs version when tecs uninstalled
    Change-Id: I1a0d8f7cca80d796901dfa4fd8c25c321d18e616

---
+++ code/daisy/daisy/tests/api/test_members.py
@@ -0,0 +1,32 @@
+import mock
+import webob
+from daisy.context import RequestContext
+from daisy import test
+from daisy.api.v1 import members
+
+
+class TestMembers(test.TestCase):
+    def setUp(self):
+        super(TestMembers, self).setUp()
+        self.controller = members.Controller()
+
+    @mock.patch('daisy.api.backends.common._judge_ssh_host')
+    @mock.patch('daisy.registry.client.v1.api.update_host_metadata')
+    @mock.patch('daisy.registry.client.v1.api.delete_cluster_host')
+    @mock.patch('daisy.registry.client.v1.api.get_host_metadata')
+    @mock.patch('daisy.registry.client.v1.api.get_cluster_metadata')
+    def test_delete_cluster_host(self, mock_get_cluster, mock_get_host,
+                                 mock_delete_cluster_host, mock_update_host,
+                                 mock_judge_ssh_host):
+        req = webob.Request.blank('/')
+        req.context = RequestContext(is_admin=True, user='fake user',
+                                     tenant='fake tenant')
+        cluster_id = '1'
+        host_id = '1'
+        mock_get_cluster.return_value = {'id': '1', 'deleted': 0}
+        mock_get_host.return_value = {'id': '1', 'deleted': 0}
+        mock_delete_cluster_host.return_value = []
+        mock_judge_ssh_host.return_value = False
+        mock_update_host.return_value = {}
+        self.controller.delete_cluster_host(req, cluster_id, host_id)
+        self.assertTrue(mock_update_host.called)
