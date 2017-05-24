81a2415757dee02012b76fd0a8d1e7075ea6ee57
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2017-05-24 06:21:32 -0400

    Slove H103
    Change-Id: Ie022898257a3f8f4aca64de8c08baf85a24248c1
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- code/daisy/daisy/db/sqlalchemy/migrate_repo/versions/010_add_share_disk_type.py
+++ code/daisy/daisy/db/sqlalchemy/migrate_repo/versions/010_add_share_disk_type.py
@@ -1,4 +1,5 @@
-# Copyright 2013 OpenStack Foundation
+# Copyright 2010 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -6,7 +7,7 @@
 #    a copy of the License at
 #
 #         http://www.apache.org/licenses/LICENSE-2.0
-# s
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
