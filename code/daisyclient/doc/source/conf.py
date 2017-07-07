4db8ed17ffd9e876fef2eff8068c5a3cc41011fa
luke.li <lilu7189@fiberhome.com>
2017-07-07 15:01:08 +0800

    Switch from oslosphinx to openstackdocstheme
    Use the new style of setting of openstackdocstheme according to follow.
    https://docs.openstack.org/openstackdocstheme/latest/
    
    Change-Id: I423842b615853444cc5d318d81628944752dd3c0

--- code/daisyclient/doc/source/conf.py
+++ code/daisyclient/doc/source/conf.py
@@ -12,7 +12,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'oslosphinx']
+extensions = ['sphinx.ext.autodoc', 'openstackdocstheme']
+
+# openstackdocstheme options
+repository_name = 'openstack/daisycloud-core'
+bug_project = 'daisycloud-core'
+bug_tag = ''
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
@@ -53,6 +59,7 @@ man_pages = [
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme = 'nature'
+html_theme = 'openstackdocs'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
