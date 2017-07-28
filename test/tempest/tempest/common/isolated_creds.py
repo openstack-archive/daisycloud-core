0ce4d549ac4cd2adbf5ab01c96787f6e03407ec5
rajat29 <rajat.sharma@nectechnologies.in>
2017-07-28 10:05:54 +0530

    Use tempest instead of Tempest-lib
    tempest-lib has been deprecated since a year. All
    the interfaces available in tempest-lib are now available
    in tempest/lib.
    
    Change-Id: I4396170abc9118235265890469e8d5f72e29a8e7

--- test/tempest/tempest/common/isolated_creds.py
+++ test/tempest/tempest/common/isolated_creds.py
@@ -16,8 +16,8 @@ import abc
 import netaddr
 from oslo_log import log as logging
 import six
-from tempest_lib.common.utils import data_utils
-from tempest_lib import exceptions as lib_exc
+from tempest.lib.common.utils import data_utils
+from tempest.lib import exceptions as lib_exc
 
 from tempest import clients
 from tempest.common import cred_provider
