0e8bb8c6e13fb27e1e92ed1e11cc17ad091d538d
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2017-09-06 23:42:21 -0400

    Add more log for debuging prepare.sh failure
    Cherry-pick from 0cc7b4d6fc597f39865270df7941aba1f65106a7
    
    Change-Id: Ib8c4e27e3b7d9455c1db66c807d6e63e9e64b9c1

--- backend/kolla/prepare.sh
+++ backend/kolla/prepare.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
+##############################################################################
+# Copyright (c) 2016 ZTE Coreporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+set -e
+
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 daisy_management_ip=$1
 yum -y install epel-release
+
 #curl -sSL https://get.docker.io | bash
 yum remove -y docker-engine
 yum install -y https://mirrors.nju.edu.cn/docker/yum/repo/centos7/Packages/docker-engine-17.05.0.ce-1.el7.centos.x86_64.rpm
+[ "$?" -ne 0 ] && { exit 1; }
+
 mkdir -p /etc/systemd/system/docker.service.d
 config_path=/etc/systemd/system/docker.service.d/kolla.conf
 touch /etc/sysconfig/docker
