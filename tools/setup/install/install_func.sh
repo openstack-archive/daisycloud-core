7e546ff90c338dd9e72ca9dd250be2e81e50878f
Zhijiang Hu <hu.zhijiang@zte.com.cn>
2017-11-16 02:16:40 -0500

    Switch ceilometer backend from mongodb to gnocchi
    This PS also update required image to a image which
    includes gnocchi.
    
    Change-Id: I5b6343d6e8b4cd170e6616521040330b3283b434
    Signed-off-by: Zhijiang Hu <hu.zhijiang@zte.com.cn>

--- tools/setup/install/install_func.sh
+++ tools/setup/install/install_func.sh
@@ -99,7 +99,7 @@ function kolla_install
   check_and_install_rpm python2-jinja2
 
   imagebranch="pike"
-  imageversion="171031195331"
+  imageversion="171116161159"
   imageserver="http://artifacts.opnfv.org/daisy/upstream"
   imagedir="/var/lib/daisy/versionfile/kolla"
   imagebakdir="/home/kolla_install/docker/"
