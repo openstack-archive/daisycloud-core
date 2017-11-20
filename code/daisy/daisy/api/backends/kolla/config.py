8e3decd6f3477a977352509668a9393a6c11301e
00184532 <lu.yao135@zte.com.cn>
2017-11-20 16:23:52 +0800

    support dpdk
    Change-Id: If0dc3a869da9fe20513f7005f0c5e072f9e9bbc0
    Signed-off-by: 00184532 <lu.yao135@zte.com.cn>

--- code/daisy/daisy/api/backends/kolla/config.py
+++ code/daisy/daisy/api/backends/kolla/config.py
@@ -311,6 +311,17 @@ def update_globals_yml(config_data, multicast_flag):
               default_flow_style=False)
 
 
+def enable_openswitch_dpdk():
+    openswitch_dpdk_config = {
+        'enable_ovs_dpdk': 'yes',
+        'neutron_bridge_name': 'dpdk_bridge',
+        'tunnel_interface': 'dpdk_bridge',
+        'enable_openvswitch': 'yes',
+        'ovs_hugepage_mountpoint': '/mnt/huge_1GB',
+        'pmd_coremask': '0x1'}
+    update_kolla_globals_yml(openswitch_dpdk_config)
+
+
 def update_password_yml():
     LOG.info(_("begin to update kolla's passwd.yml file..."))
     cmd = 'python '\
